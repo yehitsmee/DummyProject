9c1fc07a098cde21fc4bff8a07af6e9c39791950
yehitsmee <chaithra2900@gmail.com>
2025-06-24 10:01:22 +0530

    Initial Commit

---
+++ DummyProject/DummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyProjectApp.swift
+//  DummyProject
+//
+//  Created by Chaithra Nayak on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
